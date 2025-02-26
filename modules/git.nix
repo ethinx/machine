38bdf9f362601935ce5bd817a6523de0b16994cf
York Wong <965612+ethinx@users.noreply.github.com>
2025-02-26 19:51:28 +0800

    git sign

--- modules/git.nix
+++ modules/git.nix
@@ -36,9 +36,13 @@
           user = {
             name = "York Wong";
             email = "97939737+nixff@users.noreply.github.com";
+            signingkey = "~/.ssh/id_ed25519_nixff.pub";
           };
           core = {
             sshCommand = "ssh -i ~/.ssh/id_ed25519_nixff";
+          };
+          gpg = {
+            format = "ssh";
           };
         };
         condition = "gitdir:~/repo/nixff/";
